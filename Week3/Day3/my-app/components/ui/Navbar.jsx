aac7a7c6a03c0aec140b5f8478f01d552e3af835
Manya Sharma <manyas.hestabit@gmail.com>
2025-11-22 14:14:28 +0530

    Day3 added

--- Week3/Day3/my-app/components/ui/Navbar.jsx
+++ Week3/Day3/my-app/components/ui/Navbar.jsx
@@ -1,35 +1,3 @@
-// import React from "react";
-// import Input from "./Input";
-// import { FaBars, FaSearch, FaUser } from "react-icons/fa";
-
-// function Navbar({ toggleSidebar }) {
-//   return (
-//     <div className="fixed top-0 left-0 w-full bg-gray-900 h-16 px-4 flex items-center justify-between z-10">
-//       <button
-//         type="button"
-//         className="text-gray-300 hover:text-white text-xl"
-//         onClick={toggleSidebar}
-//       >
-
-//         <FaBars />
-//       </button>
-
-      
-
-//       <div className="flex gap-4 h-[67%] w-[30%] min-w-60 justify-between">
-//         <Input />
-//         <button
-//           type="button"
-//           className="text-gray-300 hover:text-white text-xl w-[10%]"
-//         >
-//           <FaUser />
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-// export default Navbar;
-
 import React from "react";
 import Input from "./Input";
 import { FaBars, FaSearch, FaUser } from "react-icons/fa";
